f9cdd4eefc94777ac9ff84cb1feee7ac9a501bdc
mrbabbs <giovanni.esposito@shore.com>
2016-09-24 19:10:08 +0200

    feat: Add percenteage module

---
+++ src/Percentage.js
@@ -0,0 +1,37 @@
+import { privateEnv } from './helper/helpers';
+import {
+  DEFAULT_PERCENTAGE_OPTIONS_TEXT,
+  DEFAULT_PERCENTAGE_PROPERTIES,
+} from './constants';
+
+const _private = privateEnv();
+
+class Percentage {
+  constructor(paper, { text, properties } = {}) {
+    const {
+      x,
+      y,
+      text: defaultText,
+    } = { ...DEFAULT_PERCENTAGE_OPTIONS_TEXT, ...text };
+    const optionsProperties = {
+      ...DEFAULT_PERCENTAGE_PROPERTIES,
+      ...properties,
+    };
+
+    _private(this).optionsText = { x, y, defaultText };
+    _private(this).percentage = paper.text(x, y, defaultText);
+
+    _private(this).percentage.attr(optionsProperties);
+  }
+
+  set value(value) {
+    _private(this).percentage.attr({ text: `${Math.round(value)}%` });
+    _private(this).value = value;
+  }
+
+  get value() { return _private(this).value; }
+
+  get optionsText() { return _private(this.optionsText); }
+}
+
+export default Percentage;
